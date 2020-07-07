7ca923a642d23daa1c7dd5ec503b553675516a0f
ehpub <jejutour@daum.net>
2020-07-07 13:54:16 +0900

    랜덤 값 맞추기
     이번 강의에서는 컴퓨터가 생각한 랜덤 값(0~99)을 사용자가 10번 시도 내에 맞추는 프로그램을 만들어 봅시다.
    
     먼저 문제를 해결하기 위한 논리를 생각하고 의사 코드(pseudo code)로 표현해 보세요.
    
    유튜브:https://youtu.be/cxPP-fKhfoE
    
    언제나 휴일:http://ehpub.co.kr/%ec%8b%a4%ec%8a%b5%ec%9c%bc%eb%a1%9c-%eb%8b%a4%ec%a7%80%eb%8a%94-c-8-%eb%9e%9c%eb%8d%a4-%ea%b0%92-%eb%a7%9e%ec%b6%94%ea%b8%b0/

---
+++ 랜덤 값 맞추기/Program.cs
@@ -0,0 +1,48 @@
+﻿//ehpub.co.kr
+//실습으로 다지는 C#
+//8. 랜던 값 맞추기
+using System;
+
+namespace _008_랜덤_값_맞추기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("0~99 사이의 정수 중에 컴퓨터가 생각한 수를 맞춰보세요.");
+            Console.WriteLine("*** 10회 안에 맞춰야 성공입니다.");
+
+            int rnum = random.Next(100);//rnum:= 랜덤 값
+            int tcnt;
+            int guess;
+            for(tcnt=1; tcnt<=10; tcnt++)//반복(tcnt: 1->10)
+            {
+                Console.Write("{0}번째 시도, 예상값:", tcnt);
+                int.TryParse(Console.ReadLine(), out guess);//guess:= 추측값 입력
+                if(guess == rnum)//조건(guess is equals rnum)
+                {
+                    break;//반복문 탈출
+                }
+                if(guess>rnum)//조건(guess > rnum)
+                {
+                    Console.WriteLine("더 작습니다.");//더 작다고 알려준다.
+                }
+                else//아니면
+                {
+                    Console.WriteLine("더 큽니다.");//더 크다고 알려준다.
+                }
+            }
+
+            if(tcnt<=10)//조건(시도 횟수가 10보다 작거나 같으면)
+            {
+                Console.WriteLine("{0}번 시도하여 정답을 맞추셨군요", tcnt);//시도 횟수와 성공하였음을 알려준다.
+            }
+            else//아니면
+            {
+                Console.WriteLine("실패하였습니다.");//실패하였다고 알려준다.
+            }
+            Console.WriteLine("컴퓨터가 생각한 정수는 {0}", rnum);
+        }
+    }
+}
