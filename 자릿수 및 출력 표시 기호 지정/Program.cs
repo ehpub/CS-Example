8f2748ba8fe742a00f0fb997ab10a4772e4e52ca
ehpub <jejutour@daum.net>
2020-07-07 12:45:09 +0900

    자릿수 및 출력 표시 기호 지정
    이번 강의에서는 숫자를 출력할 때 정수와 소수점 이하 자릿수를 지정하는 방법을 알아볼거예요.
    
     다음은 자릿수를 지정하는 표현과 예를 간략하게 나타낸 것입니다.
    
    유튜브: https://youtu.be/eXfogm7jLQI
    
    언제나휴일: https://ehpub.co.kr/003-c-%ec%a0%95%ec%88%98%ec%99%80-%ec%86%8c%ec%88%98%ec%a0%90-%ec%9d%b4%ed%95%98-%ec%9e%90%eb%a6%bf%ec%88%98%eb%a5%bc-%ec%a7%80%ec%a0%95%ed%95%98%ec%97%ac-%ec%b6%9c%eb%a0%a5/

---
+++ 자릿수 및 출력 표시 기호 지정/Program.cs
@@ -0,0 +1,32 @@
+﻿//http://ehpbu.co.kr
+//3. 정수와 소수점 이하 자릿수 결정하기
+using System;
+
+namespace _003_정수와_소수점_이하_자릿수_결정하기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dv = 1234.123456789;
+            int iv = 123;
+
+            //0의 개수 = 정수 자릿수(빈 자리는 0출력)
+            Console.WriteLine("{0:000000}", iv);
+            Console.WriteLine("{0:000000}", dv);
+
+            //"D정수" = 정수 자릿수(빈 자리는 0출력)
+            Console.WriteLine("{0:D6}", iv);
+            //Console.WriteLine("{0:D6}", dv); 실수 표현은 인자로 올 수 없다.
+
+            //"." 뒤에 0의 갯수 = 소수점 이하 자릿수
+            Console.WriteLine("{0:.000000}", dv);
+
+            //"N정수", "F정수" = 소수점 이하 자릿수
+            Console.WriteLine("{0:N}", dv);
+            Console.WriteLine("{0:N4}", dv);
+            Console.WriteLine("{0:F}", dv);
+            Console.WriteLine("{0:F5}", dv);
+        }
+    }
+}
