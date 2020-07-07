873bb209570ac9d9d7555e25445611b05339104b
ehpub <jejutour@daum.net>
2020-07-07 13:07:02 +0900

    조건문 if else
     이번 강의부터 제어문에 관한 실습을 진행할 거예요.
    
     제어문 중에서도 이번 실습에서는 조건문 if else에 관한 실습입니다.
    
      1. 두 개의 정수를 입력받은 후에 큰 수를 출력하는 프로그램을 작성하시오.
    
     2. 두 개의 정수를 입력받은 후에 작은 수가 큰 수의 약수인지 확인하는 프로그램을 작성하시오.
    
     3. 세 개의 정수를 입력받은 후에 크기가 큰 순서부터 출력하는 프로그램을 작성하시오.
    
    프로그래밍을 할 때 해야 할 작업을 바로 코드로 작성하는 것은 초반에 조급함을 달래줄 수는 있을 거예요. 하지만 실제 작업을 완료하려면 많은 시행 착오로 비용이 많이 들어갈 수 있어요.
    
     가급적이면 해야 할 작업을 의사코드(psuedo code)나 다이어그램 등을 이용하여 논리를 전개해 본 후에 타당하다면 코드로 옮기는 습관을 가져보세요. 이와 같이 작업하면 초반에 작업이 더딘 느낌이 들 거예요. 하지만 실제 작업을 완료하기 위해 들어가는 비용은 줄어들 것이라 생각합니다.
    
    유튜브: https://youtu.be/PvZpmqPt3Q8
    
    언제나휴일:http://ehpub.co.kr/%ec%8b%a4%ec%8a%b5%ec%9c%bc%eb%a1%9c-%eb%8b%a4%ec%a7%80%eb%8a%94-c-4-%ec%a1%b0%ea%b1%b4%eb%ac%b8if-else-%ec%8b%a4%ec%8a%b51-%eb%91%90-%ea%b0%9c%ec%9d%98-%ec%a0%95%ec%88%98%eb%a5%bc-%ec%9e%85/

---
+++ 조건문 if else 연습/Program.cs
@@ -0,0 +1,136 @@
+﻿//ehpub.co.kr
+//실습으로 다지는 C#
+//4. 조건문 if else 연습
+//  1. 두 개의 정수를 입력받은 후에 큰 수를 출력하는 프로그램을 작성하시오.
+//  2. 두 개의 정수를 입력받은 후에 작은 수가 큰 수의 약수인지 확인하는 프로그램을 작성하시오.
+//  3. 세 개의 정수를 입력받은 후에 크기가 큰 순서부터 출력하는 프로그램을 작성하시오.
+using System;
+
+
+namespace _004_조건문_if_else_연습
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ex1();
+            Ex2();
+            Ex3();    
+        }
+
+        private static void Ex3()
+        {
+            //  3. 세 개의 정수를 입력받은 후에 크기가 큰 순서부터 출력하는 프로그램을 작성하시오.
+            int num1, num2, num3;
+            Console.WriteLine("입력한 세 개의 정수를 크기 순으로 출력합니다.");
+            Console.Write("첫 번째 정수:");
+            if (int.TryParse(Console.ReadLine(), out num1) == false)
+            {
+                Console.WriteLine("에러 발생!!! 정수를 입력하지 않았습니다.");
+                return;
+            }
+            Console.Write("두 번째 정수:");
+            if (int.TryParse(Console.ReadLine(), out num2) == false)
+            {
+                Console.WriteLine("에러 발생!!! 정수를 입력하지 않았습니다.");
+                return;
+            }
+            Console.Write("세 번째 정수:");
+            if (int.TryParse(Console.ReadLine(), out num3) == false)
+            {
+                Console.WriteLine("에러 발생!!! 정수를 입력하지 않았습니다.");
+                return;
+            }
+
+            //num1과 num2의 값 중에 큰 값을 num1으로 설정하기
+            if(num1<num2)
+            {
+                int temp = num1;
+                num1 = num2;
+                num2 = temp;
+            }
+
+            //num1과 num3의 값 중에 큰 값을 num1으로 설정하기
+            if(num1<num3)
+            {
+                int temp = num1;
+                num1 = num3;
+                num3 = temp;
+            }
+
+            //num2와 num3의 값 중에 큰 값을 num2로 설정하기
+            if(num2<num3)
+            {
+                int temp = num2;
+                num2 = num3;
+                num3 = temp;
+            }
+            Console.WriteLine("{0} {1} {2}", num1, num2, num3);
+        }
+
+        private static void Ex2()
+        {
+            //  2. 두 개의 정수를 입력받은 후에 작은 수가 큰 수의 약수인지 확인하는 프로그램을 작성하시오.
+            int num1, num2;
+            Console.WriteLine("입력한 두 개의 정수 중에 작은 수가 큰 수의 약수인지 판별합니다.");
+            Console.Write("첫 번째 정수:");
+            if (int.TryParse(Console.ReadLine(), out num1) == false)
+            {
+                Console.WriteLine("에러 발생!!! 정수를 입력하지 않았습니다.");
+                return;
+            }
+            Console.Write("두 번째 정수:");
+            if (int.TryParse(Console.ReadLine(), out num2) == false)
+            {
+                Console.WriteLine("에러 발생!!! 정수를 입력하지 않았습니다.");
+                return;
+            }
+            if (num1 < num2)
+            {
+                int temp = num1;
+                num1 = num2;
+                num2 = temp;
+            }
+
+            if(num1%num2 == 0)
+            {
+                Console.WriteLine("{0}은 {1}의 약수입니다.", num2, num1);
+            }
+            else
+            {
+                Console.WriteLine("{0}은 {1}의 약수가 아닙니다.", num2, num1);
+            }
+        }
+
+        private static void Ex1()
+        {
+            //  1. 두 개의 정수를 입력받은 후에 큰 수를 출력하는 프로그램을 작성하시오.
+            int num1, num2;
+            Console.WriteLine("입력한 두 개의 정수 중에 큰 수를 출력합니다.");
+            Console.Write("첫 번째 정수:");
+            if(int.TryParse(Console.ReadLine(),out num1) == false)
+            {
+                Console.WriteLine("에러 발생!!! 정수를 입력하지 않았습니다.");
+                return;
+            }
+            Console.Write("두 번째 정수:");
+            if (int.TryParse(Console.ReadLine(), out num2) == false)
+            {
+                Console.WriteLine("에러 발생!!! 정수를 입력하지 않았습니다.");
+                return;
+            }
+            if(num1>num2)
+            {
+                Console.WriteLine("{0}이 {1}보다 큰 정수입니다.", num1, num2);
+            }
+            else if(num1==num2)
+            {
+                Console.WriteLine("두 개의 정수는 모두 {0}로 같습니다.", num1);
+            }
+            else
+            {
+                Console.WriteLine("{1}이 {0}보다 큰 정수입니다.", num1, num2);
+            }
+        }
+    }
+}
