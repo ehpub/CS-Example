50eafe96dcebc488bbe2c669c1da078da47d94c0
ehpub <jejutour@daum.net>
2020-07-07 12:19:58 +0900

    숫자 출력 포멧
    이번 실습에서는 형식 지정자를 이용하여 숫자를 출력하는 방법을 알아보기로 할게요.
     이미 이전 강의에서는 출력할 때 문자열 출력 포멧을 알아보았죠.
    {index[,alignment][:formatString]}
    {인덱스[,배치][:포멧문자열]}
    
    포멧 문자열에는 출력할 내용을 어떠한 형식으로 출력할 것이지 나타내는 표현도 나타낼 수 있습니다. 이를 형식 지정자라 불러요.
    이번 실습에서는 형식 지정자를 이용하여 숫자를 표현하는 방법을 다룰거예요.
    유튜브: https://youtu.be/YzWjrAwkO2Q\
    언제나휴일: http://ehpub.co.kr/002-%ec%88%ab%ec%9e%90-%ec%b6%9c%eb%a0%a5-%ed%98%95%ec%8b%9d-%ec%a7%80%ec%a0%95%ec%9e%90-%ec%9d%b4%ec%9a%a9%ed%95%98%ea%b8%b0/

---
+++ 숫자 출력 포멧/Program.cs
@@ -0,0 +1,38 @@
+﻿//http://ehpub.co.kr
+//002. 숫자 출력 - 형식 지정자 이용하기
+using System;
+namespace _002_숫자_출력_포멧
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iv = 12345;
+            double dv = 12345.6789;
+            double dv2 = 0.23;
+            //D: 10 진수
+            Console.WriteLine("{0:D}", iv);
+            Console.WriteLine(iv.ToString("D"));
+            //E: 지수
+            Console.WriteLine("{0:E}",dv);
+            Console.WriteLine(dv.ToString("E"));
+            //F: 고정 소수점
+            Console.WriteLine("{0:F}", dv);
+            Console.WriteLine(dv.ToString("F"));
+            //G:일반
+            Console.WriteLine("{0:G}", dv);
+            Console.WriteLine(dv.ToString("G"));
+            Console.WriteLine("{0:G}", 0.0000023);
+            //N:숫자
+            Console.WriteLine("{0:N}", 123456789);
+            //P: 백분율
+            Console.WriteLine("{0:P}", dv2);
+            Console.WriteLine(dv2.ToString("P"));
+            //X: 16진수
+            Console.WriteLine("0x{0:X}", iv);
+            Console.WriteLine("0x{0}", iv.ToString("X"));
+        }
+    }
+}
+
+
