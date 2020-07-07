01882213fbf31a758a8a7ea39904ab0c0e861295
ehpub <jejutour@daum.net>
2020-07-07 10:30:43 +0900

    문자열 출력 포멧
    유튜브: https://youtu.be/ewsh61FVZgo
    언휴:http://ehpub.co.kr/001-c-%eb%ac%b8%ec%9e%90%ec%97%b4-%ec%b6%9c%eb%a0%a5-%ed%8f%ac%eb%a9%a7/

---
+++ 001 문자열 출력 포멧/001 문자열 출력 포멧/Program.cs
@@ -0,0 +1,26 @@
+﻿//https://ehpub.co.kr
+//실습으로 다지는 C#
+//  001. 문자열 출력 포멧
+//      {index;,alignment][:formatString]}
+//      {인덱스[,배치][:포멧문자열]}
+
+using System;
+
+namespace _001_문자열_출력_포멧
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("테스트1: {0}, {1}, {2}", "Hello", 23.7, 10);
+            Console.WriteLine("테스트2: {0}, {2}, {1}", "Hello", 23.7, 10);
+
+            //배치
+            Console.WriteLine("테스트3:{0,10}, {0,-10}끝", 1234);
+
+            //포멧 문자열
+            Console.WriteLine("테스트4: 0x{0:X}, {0:E}, {0:N}", 12345678);
+        }
+    }
+}
