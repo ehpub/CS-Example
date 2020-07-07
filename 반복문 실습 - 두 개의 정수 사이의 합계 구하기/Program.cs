3f104a401461ac57b847325d70ce093a2161ae6b
ehpub <jejutour@daum.net>
2020-07-07 13:37:43 +0900

    두 개의 정수 사이의 합계 구하기
     이번 강의에서는 두 개의 정수를 입력 받은 후에 두 개의 정수 사이의 합계를 구하는 실습을 해 봅시다.
    
     먼저 어떻게 해야 문제를 해결할 수 있을지 생각한 후에 의사 코드(pseudo code) 형태로 작성해 보세요. 작성한 후에 비교해 보시기 바랍니다.
    
    유튜브:https://youtu.be/PFB0Q6rP-W0
    
    언제나 휴일:http://ehpub.co.kr/%ec%8b%a4%ec%8a%b5%ec%9c%bc%eb%a1%9c-%eb%8b%a4%ec%a7%80%eb%8a%94-c-6-%eb%b0%98%eb%b3%b5%eb%ac%b8-%ec%8b%a4%ec%8a%b51-%eb%91%90-%ea%b0%9c%ec%9d%98-%ec%a0%95%ec%88%98-%ec%82%ac%ec%9d%b4%ec%9d%98/

---
+++ 반복문 실습 - 두 개의 정수 사이의 합계 구하기/Program.cs
@@ -0,0 +1,58 @@
+﻿//http://ehpub.co.kr
+//실습으로 다지는 C#
+//5. 반복문 실습1
+//두 개의 정수를 입력받은 후에 두 수 사이의 모든 정수의 합계 구하기
+using System;
+
+namespace _005_반복문_실습1___두_개의_정수를_입력받은_후에_두_수_사이의_모든_정수의_합계_구하기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("첫 번째 정수를 입력하세요.");
+            string input = Console.ReadLine();//input:= 정수를 입력 받는다.
+            int num1;
+            if(int.TryParse(input,out num1)==false)//조건(정수로 변환(input, out num1)이 실패)
+            {
+                Console.WriteLine("정수가 아닙니다.");//    오류를 출력 후 알고리즘 종료
+                return;
+            }
+
+            Console.WriteLine("두 번째 정수를 입력하세요.");
+            input = Console.ReadLine();//input := 정수를 입력 받는다.
+            int num2;
+            if(int.TryParse(input,out num2) == false)//조건(정수로 변환(input, out num2)이 실패)
+            {
+                Console.WriteLine("정수가 아닙니다.");//    오류를 출력 후 알고리즘 종료
+                return;
+            }
+
+            if(num1<num2)//조건(num1 < num2)
+            {
+                Swap(ref num1, ref num2);//    두 수를 교환(ref num1, ref num2)
+            }
+
+            int sum = CalSum(num2, num1);   //sum:= 두 정수 사이의 합계 구하기(num2, num1)
+            Console.WriteLine("{0}에서 {1}사이의 합계는 {2}입니다.", num2, num1, sum);
+        }
+
+        //두 수를 교환(ref num1, ref num2)
+        static void Swap(ref int num1, ref int num2)
+        {
+            int temp = num1;//temp:= num1
+            num1 = num2;//num1:= num2
+            num2 = temp;//num2:= temp
+        }
+        //두 정수 사이의 합계 구하기(start, end):합계
+        static int CalSum(int start, int end)
+        {
+            int sum = 0;//sum:= 0
+            for (int value = start; value <= end; value++)//반복(value:= start->end)
+            {
+                sum += value;//    sum:= sum + value
+            }
+            return sum;//sum 반환            
+        }
+    }
+}
